2fced17450403b4c427528ccf40083af1468a44c
cokoyoh <charlesokoyoh@gmail.com>
2018-01-18 20:04:11 +0300

    Install passport and obtain access tokens

--- app/User.php
+++ app/User.php
@@ -2,12 +2,18 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    use SoftDeletes;
+
+    use HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
--- resources/assets/js/components/authentication/Login.vue
+++ resources/assets/js/components/authentication/Login.vue
@@ -4,20 +4,28 @@
             <div class = "large-6 medium-6 large-offset-3 medium-offset-2 columns">
                 <div class = "medium-12 cell">
                     <label>Email
-                        <input type = "text" placeholder = "xyz@cytonn.com"
-                               name = "email" id = "email">
+                        <input type = "text"
+                               placeholder = "xyz@cytonn.com"
+                               name = "email"
+                               id = "email"
+                               v-model="login.email"
+                        >
                     </label>
 
                 </div>
                 <div class = "medium-12 cell">
                     <label>Password
-                        <input type = "password" placeholder = "* * * * * *"
-                               name = "password" id = "password">
+                        <input type = "password"
+                               placeholder = "* * * * * *"
+                               name = "password"
+                               id = "password"
+                               v-model="login.password"
+                        >
                     </label>
 
                 </div>
                 <div class = "login-button medium-12 cell">
-                    <button class = "button medium" type = "submit"><i class="fa fa-power-off"></i> Login</button>
+                    <button class = "button medium" type = "submit" @click="submit"><i class="fa fa-power-off"></i> Login</button>
                 </div>
             </div>
         </div>
@@ -26,11 +34,37 @@
 <script>
     export default {
         data() {
-            return {}
+            return {
+                login : {
+                    email: 'john@example.com',
+                    password: 'secret',
+                }
+            }
         },
         created()
         {
             console.log("Login Created");
+        },
+        methods: {
+            submit()
+            {
+                let postData = {
+                    client_id: 2,
+
+                    client_secret: 'Ma814kaJ15hugE6uRwqxNJRp4XUSlgVjfokTpcxl',
+
+                    grant_type: 'password',
+
+                    username: this.login.email,
+
+                    password: this.login.password,
+                };
+
+                axios.post('/oauth/token', postData)
+                    .then(response => {
+                        console.log(response);
+                    })
+            }
         }
     }
 
